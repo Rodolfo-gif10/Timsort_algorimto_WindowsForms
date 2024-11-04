e98061095e3d6bf3905b5d527877d7aac10ea4ce
Rodolfo-gif10 <nombrecualquier821@gmail.com>
2024-11-04 16:33:52 -0600

    primer commit
    formulario con un texboxt, un datagriedview y button e implementacion de timsort

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Timsort_EstructuraII_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.dataGridViewNombres = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewNombres)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(22, 12);
+            this.txtNombres.Multiline = true;
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(100, 735);
+            this.txtNombres.TabIndex = 0;
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(141, 185);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(75, 84);
+            this.btnOrdenar.TabIndex = 1;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click_1);
+            // 
+            // dataGridViewNombres
+            // 
+            this.dataGridViewNombres.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewNombres.Location = new System.Drawing.Point(237, 12);
+            this.dataGridViewNombres.Name = "dataGridViewNombres";
+            this.dataGridViewNombres.RowHeadersWidth = 51;
+            this.dataGridViewNombres.RowTemplate.Height = 24;
+            this.dataGridViewNombres.Size = new System.Drawing.Size(199, 735);
+            this.dataGridViewNombres.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 759);
+            this.Controls.Add(this.dataGridViewNombres);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.txtNombres);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewNombres)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.DataGridView dataGridViewNombres;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Timsort_EstructuraII_2    
+{
+    public partial class Form1 : Form
+    {
+        private const int RUN = 32;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Configuración del DataGridView
+            dataGridViewNombres.ColumnCount = 1;
+            dataGridViewNombres.Columns[0].Name = "Nombres";
+            dataGridViewNombres.AllowUserToAddRows = false;
+        }
+
+        private void InsertionSort(string[] arreglo, int izquierda, int derecha)
+        {
+            for (int i = izquierda + 1; i <= derecha; i++)
+            {
+                string clave = arreglo[i];
+                int j = i - 1;
+
+                while (j >= izquierda && arreglo[j].CompareTo(clave) > 0)
+                {
+                    arreglo[j + 1] = arreglo[j];
+                    j--;
+                }
+                arreglo[j + 1] = clave;
+            }
+        }
+
+        private void Merge(string[] arreglo, int izquierda, int medio, int derecha)
+        {       //se suma 1 para incluir el elemento medio
+            int longitud1 = medio - izquierda + 1;
+            int longitud2 = derecha - medio;
+            string[] arregloIzquierda = new string[longitud1];
+            string[] arregloDerecha = new string[longitud2];
+
+            Array.Copy(arreglo, izquierda, arregloIzquierda, 0, longitud1);
+            Array.Copy(arreglo, medio + 1, arregloDerecha, 0, longitud2);
+
+            int i = 0, j = 0, k = izquierda;
+
+            while (i < longitud1 && j < longitud2)
+            {
+               
+                if (arregloIzquierda[i].CompareTo(arregloDerecha[j]) <= 0)
+                {
+                    arreglo[k++] = arregloIzquierda[i++];
+                }
+                else
+                {
+                    arreglo[k++] = arregloDerecha[j++];
+                }
+            }
+            // ciclo para copiar cualquier elemento restante en
+            // arregloIzquierda al arreglo original
+            while (i < longitud1)
+            {
+                //Agrega los elementos restantes de arregloIzquierda.
+                arreglo[k++] = arregloIzquierda[i++];
+            }
+
+            while (j < longitud2)
+            {
+                arreglo[k++] = arregloDerecha[j++];
+            }
+        }
+
+        private void TimSort(string[] arreglo)
+        {   // n captura la cantidad de elementos del arreglo
+            int n = arreglo.Length;  
+
+            // Realiza InsertionSort en bloques de tamaño RUN
+            for (int i = 0; i < n; i += RUN)
+            {                              
+                InsertionSort(arreglo, i, Math.Min(i + RUN - 1, n - 1));
+            }
+
+            // Mezcla los bloques               
+            for (int tamaño = RUN; tamaño < n; tamaño *= 2)
+            {
+                for (int izquierda = 0; izquierda < n; izquierda += 2 * tamaño)
+                {
+                    int medio = izquierda + tamaño - 1;
+                    int derecha = Math.Min((izquierda + 2 * tamaño - 1), (n - 1));
+
+                    if (medio < derecha)
+                    {
+                        Merge(arreglo, izquierda, medio, derecha);
+                    }
+                }
+            }
+        }
+
+
+        private void btnOrdenar_Click_1(object sender, EventArgs e)
+        {
+            string[] nombres = txtNombres.Text.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            TimSort(nombres);
+
+            // Limpiar el DataGridView
+            dataGridViewNombres.Rows.Clear();
+
+            // Agregar los nombres ordenados al DataGridView
+            foreach (var nombre in nombres)
+            {
+                dataGridViewNombres.Rows.Add(nombre);
+            }
+        }
+    }
+}
